6752ed3c7511af33e6cca2bf793f10a164d68f3e
Roman Martyanov <iromich98@gmail.com>
2018-04-14 13:28:14 +0300

    Updated conception of 'Remind'
    'Remind' can have remind-time and be repeatable at the same time

--- scrumban_board_python/scrumban_board/remind.py
+++ scrumban_board_python/scrumban_board/remind.py
@@ -6,8 +6,8 @@ class Remind:
     def __init__(self, title=None, description=None, when_remind=None, repeating_remind_timedelta=None):
         self.title = title
         self.description = description
-        self.when_remind = when_remind
-        self.repeating_remind_timedelta = repeating_remind_timedelta
+
+        self.is_repeatable = False
 
         if when_remind is not None:
             if isinstance(when_remind, datetime.datetime):
@@ -15,13 +15,20 @@ class Remind:
 
             elif isinstance(when_remind, str):
                 pass
+        else:
+            self.when_remind = datetime.datetime.now()
 
         if repeating_remind_timedelta is not None:
+            self.is_repeatable = True
+
             if isinstance(repeating_remind_timedelta, datetime.timedelta):
                 self.repeating_remind_timedelta = repeating_remind_timedelta
 
             elif isinstance(repeating_remind_timedelta, str):
                 pass
+
+        else:
+            self.repeating_remind_timedelta = repeating_remind_timedelta
 
         self.id = sha1((title + description + str(datetime.datetime.now())).encode('utf-8'))
 
