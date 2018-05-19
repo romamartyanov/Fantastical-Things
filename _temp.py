c459f3b7a1ec228df15b475b77f485022ccc1a93
Roman Martyanov <iromich98@gmail.com>
2018-05-20 02:10:45 +0300

    fully created calendar

--- _temp.py
+++ _temp.py
@@ -1,22 +1,48 @@
-from scrumban_board_python import scrumban_board
-
-from hashlib import sha1
+from collections import deque
 import datetime
 
-name = "name"
-surname = "surname"
+from scrumban_board_python import scrumban_board
 
-m = sha1((name+surname).encode('utf-8'))
-
-print(m.hexdigest())
-print()
-
-d = {}
-
-d[[1]] = 'a'
-d[[2]] = 'b'
-
-print(d[[1]])
+# from hashlib import sha1
+# import datetime
+#
+# name = "name"
+# surname = "surname"
+#
+# m = sha1((name+surname).encode('utf-8'))
+#
+# print(m.hexdigest())
+# print()
+#
+# d = {}
+#
+# d[[1]] = 'a'
+# d[[2]] = 'b'
+#
+# print(d[[1]])
 
 # rem = scrumban_board.Remind()
 # rem.update_remind(title=name, description=surname, when_remind="d")
+# a = list([scrumban_board.User(name="1"), scrumban_board.User(name="2")])
+#
+# print(next(user for user in a if user.name == "3"))
+
+# a = deque()
+#
+# a.append(2)
+# a.append(4)
+# a.append(5)
+# print(a)
+#
+# a.insert(a.index(4), 3)
+# print(a)
+#
+# for i in a:
+#     print(i)
+
+a = datetime.datetime.now()
+b = datetime.datetime(2017, 9, 5)
+
+if a > b:
+    print(a)
+
