cd2637cbdc299a72708089ab5720edfebfba9257
Roman Martyanov <iromich98@gmail.com>
2018-05-26 01:53:00 +0300

    to Subtask added docs and logging

--- scrumban_board_python/scrumban_board/subtask.py
+++ scrumban_board_python/scrumban_board/subtask.py
@@ -5,17 +5,35 @@ from scrumban_board_python.scrumban_board.terminal_colors import Colors
 
 
 class Subtask:
+    """
+    Description of Subtask
+
+    Example:
+    task.add_subtask(scrumban_board.Subtask(client.logger, "subtask1"))
+    task.add_subtask(scrumban_board.Subtask(client.logger, "subtask2"))
+    """
+
     def __init__(self, logger, title: str,
                  description: str = None):
+        """
+        Initialising of Subtask
+
+        :param logger: client logger
+        :param title: subtask title
+        :param description: subtask description
+        """
 
         self.title = title
         self.description = description
+        self.logger = logger
 
         self.completed = False
 
         self.id = sha1(("Subtask: " + " " +
                         self.title + " " +
                         str(datetime.datetime.now())).encode('utf-8')).hexdigest()
+
+        self.logger.info("Subtask ({}) was created".format(self.id))
 
     def __str__(self):
         output = Colors.subtask_lightblue + """
@@ -48,6 +66,14 @@ ID: {}
         return output
 
     def update_subtask(self, title: str = None, description: str = None, completed: bool = None):
+        """
+        Updating of Subtask
+
+        :param title: subtask title
+        :param description: subtask description        :param description:
+        :param completed:
+        :return:
+        """
         if title is not None:
             self.title = title
 
@@ -56,3 +82,6 @@ ID: {}
 
         if completed is not None:
             self.completed = completed
+
+        self.logger.info("Subtask ({}) was updated".format(self.id))
+
