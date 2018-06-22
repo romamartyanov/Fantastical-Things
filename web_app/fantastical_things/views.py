20e100cb9a3c7cd812cb74909457b99e7ec3cda1
Roman Martyanov <iromich98@gmail.com>
2018-06-22 04:07:17 +0300

    login form created

--- web_app/fantastical_things/views.py
+++ web_app/fantastical_things/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+from django.http import *
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+
+    return render_to_response('fantastical_things/login.html', context_instance=RequestContext(request))
+
 
 def index(request):
     return render(request, 'fantastical_things/index.html')
