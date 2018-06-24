bff3da8873b06d6888fca5ebd1de43692531d15e
Roman Martyanov <iromich98@gmail.com>
2018-06-24 03:24:26 +0300

    editing all models from board page

--- web_app/fantastical_things/urls.py
+++ web_app/fantastical_things/urls.py
@@ -11,17 +11,21 @@ urlpatterns = [
     url(r'^board/(?P<board_id>\d+)/?$', views.board, name='board'),
 
     url(r'^board/(?P<board_id>\d+)/complete_task/(?P<task_id>\d+)/?$', views.complete_task, name='complete_task'),
-    # url(r'^incomplete_task/(?P<task_id>\d+)/?&', views.incomplete_task, name='incomplete_task')
 
     # url(r'^add_board/?$', views.add_board, name='add_board'),
     # url(r'^add_cardlist/?$', views.add_cardlist, name='add_cardlist'),
     # url(r'^add_card/?$', views.add_card, name='add_card'),
     # url(r'^add_task/?$', views.add_task, name='add_task'),
 
-    # url(r'^edit_board/?$', views.edit_board, name='edit_board'),
-    # url(r'^edit_cardlist/?$', views.edit_cardlist, name='edit_cardlist'),
-    # url(r'^edit_card/?$', views.edit_card, name='edit_card'),
-    # url(r'^edit_task/?$', views.edit_task, name='edit_task'),
+    url(r'^board/(?P<board_id>\d+)/edit_board/?$', views.edit_board, name='edit_board'),
+    url(r'^board/(?P<board_id>\d+)/edit_cardlist/(?P<cardlist_id>\d+)/?$', views.edit_cardlist, name='edit_cardlist'),
+    url(r'^board/(?P<board_id>\d+)/edit_card/(?P<card_id>\d+)/?$', views.edit_card, name='edit_card'),
+    url(r'^board/(?P<board_id>\d+)/edit_task/(?P<task_id>\d+)/?$', views.edit_task, name='edit_task'),
+
+    url(r'^board/(?P<board_id>\d+)/delete_board/?$', views.delete_board, name='delete_board'),
+    url(r'^board/(?P<board_id>\d+)/delete_cardlist/(?P<cardlist_id>\d+)/?$', views.delete_cardlist, name='delete_cardlist'),
+    url(r'^board/(?P<board_id>\d+)/delete_card/(?P<card_id>\d+)/?$', views.delete_card, name='delete_card'),
+    url(r'^board/(?P<board_id>\d+)/delete_task/(?P<task_id>\d+)/?$', views.delete_task, name='delete_task'),
 
     # url(r'^profile/?$', views.profile, name='profile'),
 ]
--- web_app/fantastical_things/views.py
+++ web_app/fantastical_things/views.py
@@ -65,7 +65,8 @@ def all_boards(request):
     if not request.user.is_authenticated:
         return redirect('/login/')
 
-    username = auth.get_user(request).username
+    # username = auth.get_user(request).username
+
     boards = Board.objects.filter(user=request.user)
 
     context = {
@@ -73,41 +74,6 @@ def all_boards(request):
     }
 
     return render(request, 'fantastical_things/boards_list.html', context)
-
-
-# def create_view_board_context(request, board_id):
-#     board = Board.objects.get(user=request.user, id=board_id)
-#     card_lists = board.cardlist_set.all().filter(user=request.user)
-#
-#     context = {
-#         'board': board,
-#         'card_lists': [],
-#     }
-#
-#     for card_list in card_lists:
-#
-#         card_list_dict = {
-#             'card_list': card_list,
-#             'cards': []
-#         }
-#
-#         cards = card_list.card_set.all().filter(user=request.user)
-#
-#         for card in cards:
-#             card_dict = {
-#                 'card': card,
-#                 'tasks': []
-#             }
-#
-#             tasks = card.task_set.all().filter(user=request.user)
-#
-#             card_dict['tasks'] = tasks
-#
-#             card_list_dict['cards'].append(card_dict)
-#
-#         context['card_lists'].append(card_list_dict)
-#
-#     return context
 
 
 def board(request, board_id):
@@ -147,8 +113,6 @@ def board(request, board_id):
 
         context['card_lists'].append(card_list_dict)
 
-    # context = create_view_board_context(request, board_id)
-
     return render(request, 'fantastical_things/board.html', context)
 
 
@@ -164,5 +128,135 @@ def complete_task(request, board_id, task_id):
     else:
         Task.objects.filter(user=request.user, id=task_id).update(status=True)
 
-    return redirect('/board/'+board_id)
+    return redirect('/board/' + board_id)
 
+
+def edit_board(request, board_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    if request.POST:
+        context = {}
+        context.update(csrf(request))
+
+        Board.objects.filter(user=request.user, id=board_id).update(title=request.POST['title'])
+        Board.objects.filter(user=request.user, id=board_id).update(description=request.POST['description'])
+
+        # добавить проверку
+        return redirect('/board/' + board_id)
+
+    board = Board.objects.get(user=request.user, id=board_id)
+
+    context = {
+        'board': board,
+    }
+
+    return render(request, 'fantastical_things/edit/edit_board.html', context)
+
+
+def edit_cardlist(request, board_id, cardlist_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    if request.POST:
+        context = {}
+        context.update(csrf(request))
+
+        CardList.objects.filter(user=request.user, id=cardlist_id).update(title=request.POST['title'])
+        CardList.objects.filter(user=request.user, id=cardlist_id).update(description=request.POST['description'])
+
+        # добавить проверку
+        return redirect('/board/' + board_id)
+
+    cardlist = CardList.objects.get(user=request.user, id=cardlist_id)
+
+    context = {
+        'board_id': board_id,
+        'cardlist': cardlist,
+    }
+
+    return render(request, 'fantastical_things/edit/edit_cardlist.html', context)
+
+
+def edit_card(request, board_id, card_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    if request.POST:
+        context = {}
+        context.update(csrf(request))
+
+        Card.objects.filter(user=request.user, id=card_id).update(title=request.POST['title'])
+
+        # добавить проверку
+        return redirect('/board/' + board_id)
+
+    card = Card.objects.get(user=request.user, id=card_id)
+
+    context = {
+        'board_id': board_id,
+        'card': card,
+    }
+
+    return render(request, 'fantastical_things/edit/edit_card.html', context)
+
+
+def edit_task(request, board_id, task_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    if request.POST:
+        context = {}
+        context.update(csrf(request))
+
+        Task.objects.filter(user=request.user, id=task_id).update(title=request.POST['title'])
+        Task.objects.filter(user=request.user, id=task_id).update(description=request.POST['description'])
+        Task.objects.filter(user=request.user, id=task_id).update(status=request.POST['status'])
+
+        # добавить проверку
+        return redirect('/board/' + board_id)
+
+    task = Task.objects.get(user=request.user, id=task_id)
+
+    context = {
+        'board_id': board_id,
+        'task': task,
+    }
+
+    return render(request, 'fantastical_things/edit/edit_task.html', context)
+
+
+def delete_board(request, board_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    Board.objects.filter(user=request.user, id=board_id).delete()
+
+    return redirect('/')
+
+
+def delete_cardlist(request, board_id, cardlist_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    CardList.objects.filter(user=request.user, id=cardlist_id).delete()
+
+    return redirect('/board/' + board_id)
+
+
+def delete_card(request, board_id, card_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    Card.objects.filter(user=request.user, id=card_id).delete()
+
+    return redirect('/board/' + board_id)
+
+
+def delete_task(request, board_id, task_id):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    Task.objects.filter(user=request.user, id=task_id).delete()
+
+    return redirect('/board/' + board_id)
